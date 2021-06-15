4a4a5c6dca6bad432187da94e6fb47e024713944
gourigs <71514229+gourigs@users.noreply.github.com>
2021-06-15 21:57:33 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -34,7 +34,7 @@ float average(int a[],int p)
 {
   int sum=0;
   float avg=0;
-  for(int i=0;i<n;i++)
+  for(int i=0;i<p;i++)
   {
     sum+=a[i];
   }
@@ -43,3 +43,75 @@ float average(int a[],int p)
 }
 
 //mode function//
+
+int mode(int a[],int p)
+{
+  int big=max(a,p);
+  int small=min(a,p);
+  int count[100],b=-1;
+  for(int i=small;i<=big;i++)
+  {
+   int c=0;
+   for int j=0;j<p;j++)
+   {
+    if(a[j]==i)
+      c++;
+   }
+   count[++b]=c;
+  }
+ int abig=0,d=0;
+ for(int i=0;i<=b;i++)
+ {
+  if(count[i]>abig)
+  {
+   abig=count[i];
+   d=i-(0-small);
+  }
+  else
+   continue;
+ }
+ return d;
+}
+//prime function//
+
+int isprime(int i)
+{
+ int a=0;
+ for(int j=2;j<i;j++)
+ {
+  if(i%j==0)
+  {
+   a=1;
+   break;
+  }
+  if(a==0)
+   return 1;
+  else 
+   return 0;
+ }
+  
+int factors(int p,int a[])
+{
+   int c=0,i=2;
+   while(i<p)
+  {
+   if(p%i==0)
+     if(isprime(i)==1)
+     {
+      a[c]=i;
+      c++;
+      p=p/i;
+      if(isprime(p)==1)
+       {
+       a[c++]=p;
+       break;
+       }
+      i=2;
+     }
+   else continue;
+   else
+    i++;
+  }
+  return c;
+}
+  
