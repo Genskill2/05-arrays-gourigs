f7e934d3973b5caa143aaada2b6a011851814299
gourigs <71514229+gourigs@users.noreply.github.com>
2021-06-17 18:02:54 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -2,120 +2,111 @@
 #include <stdio.h>
 //avg function//
  
-float average(int a[],int p)
+
+ int max(int a[],int n)
 {
-  int sum=0;
-  float avg=0;
-  for(int i=0;i<p;i++)
-  {
-    sum+=a[i];
-  }
-  avg=sum/p;
-  return avg;
+	int max=0;
+	for (int i=0;i<n;i++)
+	{
+		if (a[i]>max)
+			max=a[i];
+		else
+			continue;
+	}
+	return max;
 }
-// factor//
-
-int isprime(int i)
+/*min function*/
+int min(int a[],int n)
 {
- int a=0;
- for(int j=2;j<i;j++)
- {
-  if(i%j==0)
-  {
-   a=1;
-   break;
-  }
-  if(a==0)
-   return 1;
-  else 
-   return 0;
- }
-  
-int factors(int p,int a[])
-{
-   int c=0,i=2;
-   while(i<p)
-  {
-   if(p%i==0)
-     if(isprime(i)==1)
-     {
-      a[c]=i;
-      c++;
-      p=p/i;
-      if(isprime(p)==1)
-       {
-       a[c++]=p;
-       break;
-       }
-      i=2;
-     }
-   else continue;
-   else
-    i++;
-  }
-  return c;
+	int min=a[0];
+	for(int i=0;i<n;i++)
+	{
+		if(a[i]<min)
+			min=a[i];
+		else
+			continue;
+	}
+	return min;
 }
-  
- // max Function//
-int max(int a[],int p)
+float average(int a[],int n)
 {
-  int maximum=0;
-  for (int i=0;i<p;i++)
-  {
-    if (a[i]>maximum)
-        maximum=a[i];
-    else 
-      continue;
-  }
-  return maximum;
+	int sum=0;
+	float avg=0;
+	for(int i=0;i<n;i++)
+	{
+		sum+=a[i];
+	}
+	avg=sum/n;
+	return avg;
+}
+/*mode function*/
+int mode(int a[],int n)
+{
+	int lmax=max(a,n);
+	int lmin=min(a,n);
+	int counts[100],b=-1;
+	for(int i =lmin;i<=lmax;i++)
+		{
+		int cnt=0;
+		for(int j=0;j<n;j++)
+		{
+			if(a[j]==i)
+				cnt++;
+		}
+		counts[++b]=cnt;
+		}
+	int cmax=0,flag=0;
+	for (int i=0;i<=b;i++)
+	{
+		if (counts[i]>cmax)
+		{	cmax=counts[i];
+			flag=i-(0-lmin);
+		}
+		else
+			continue;
+	}
+	return flag;
+}
+    int isprime(int i)
+    {
+	int flag=0;
+			for(int j=2;j<i;j++)
+			{
+				if(i%j==0)
+				{
+					flag=1;
+					break;
+				}
+			}
+			if(flag==0)
+			    return 1;
+			else
+			    return 0;
+    }
+int factors(int n,int a[])
+{
+	int count=0;
+    	int i=2;
+    	while(i<n)
+    	{
+        	if(n%i==0)
+			if(isprime(i)==1)
+			{	a[count]=i;
+				count++;
+                		n=n/i;
+                		if(isprime(n)==1)
+                		{
+                			a[count++]=n;
+                			break;
+                		}
+                		i=2;
+			}
+		else continue;
+		else
+		i++;
+	}
+	return count;
 }
 
-//min function//
 
-int min(int a[],int p)
-{
-   int minimum=0;
-   for(int i=0;i<p;i++)
-   {
-     if (a[i]<minimum)
-       minimum=a[i];
-     else 
-         continue;
-   }
-  return minimum;
-}
-
-
-//mode function//
-
-int mode(int a[],int p)
-{
-  int big=max(a,p);
-  int small=min(a,p);
-  int count[100],b=-1;
-  int abig=0,d=0;
-  for(int i=small;i<=big;i++)
-   {
-    int c=0;
-    for (int j=0;j<p;j++)
-     {
-     if(a[j]==i)
-      c++;
-     }
-    count[++b]=c;
-   }
- 
- for(int i=0;i<=b;i++)
-  {
-   if(count[i]>abig)
-  
-      abig=count[i];
-      d=i-(0-small);
-  
-   else
-      continue;
-  }
- return d;
-}
- 
 
